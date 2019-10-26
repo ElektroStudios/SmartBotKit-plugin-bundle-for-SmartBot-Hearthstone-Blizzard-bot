922ab5d59da134ff145fe639881b9e7f29afc061
Elektro <ElektroStudios@ElHacker.Net>
2019-10-26 19:48:31 +0200

    ...

--- Libs/SmartBotKit.Core/Interop/Hearthstone/FriendChallengeType.vb
+++ Libs/SmartBotKit.Core/Interop/Hearthstone/FriendChallengeType.vb
@@ -1,33 +1,33 @@
 ﻿
 
 ' ****************************************************************************
-' This code belongs to the 'Challenge Notifier' plugin, which is discontinued.
+' This source-code belongs to the 'Challenge Notifier' plugin, which is discontinued.
 ' ****************************************************************************
 
 
-'' ReSharper disable once CheckNamespace
-'Namespace SmartBotKit.Interop
+' ReSharper disable once CheckNamespace
+Namespace SmartBotKit.Interop
 
 
-'    Public Enum FriendChallengeType
+    Public Enum FriendChallengeType
 
-'        ''' <summary>
-'        ''' A normal friend challenge.
-'        ''' </summary>
-'        Normal = 0
+        ''' <summary>
+        ''' A normal friend challenge.
+        ''' </summary>
+        Normal = 0
 
-'        ''' <summary>
-'        ''' A 80 gold quest friend challenge.
-'        ''' </summary>
-'        GoldQuest = 1
+        ''' <summary>
+        ''' A 80 gold quest friend challenge.
+        ''' </summary>
+        GoldQuest = 1
 
-'        ''' <summary>
-'        ''' Undetermined.
-'        ''' <para></para>
-'        ''' Useful for debug purposes, in scenarios on which <see cref="HearthMirrorFriendlyChallengeProvider.DialogVisible"/> is False.
-'        ''' </summary>
-'        Undetermined = -1
+        ''' <summary>
+        ''' Undetermined.
+        ''' <para></para>
+        ''' Useful for debug purposes, in scenarios on which <see cref="HearthMirrorFriendlyChallengeProvider.DialogVisible"/> is False.
+        ''' </summary>
+        Undetermined = -1
 
-'    End Enum
+    End Enum
 
-'End Namespace
+End Namespace
